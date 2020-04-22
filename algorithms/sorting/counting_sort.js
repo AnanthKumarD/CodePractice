f4bcdaa7404acddd76886b86d5c119b862a7c879
andodda <ananthkumard89@gmail.com>
2020-04-22 11:26:36 +0530

    21-02-2020 counting and mergesort integration

--- algorithms/sorting/counting_sort.js
+++ algorithms/sorting/counting_sort.js
@@ -1,16 +1,15 @@
 let countingSort = (arr, min, max) => {
     let i = min,
         j = 0,
-        len = arr.length,
-        count = [];
-    for (i; i <= max; i++) {
-        count[i] = 0;
-    }
+        len = arr.length;
+
+    var count = Array(max + 1).fill(0);
+    console.log(count);
     for (i = 0; i < len; i++) {
         count[arr[i]] += 1;
     }
     console.log(count);
-    for (i = min; i <= max; i++) {
+    for (i = min; i < max; i++) {
         while (count[i] > 0) {
             arr[j] = i;
             j++;
--- algorithms/sorting/mergeSort.js
+++ algorithms/sorting/mergeSort.js
@@ -1,23 +1,17 @@
-// var a = [12, 11, 13, 5, 6, 7];
-var a = [340, 1, 3, 3, 76, 23, 4, 12, 122, 7642, 646];
-
 function MergeSort(a) {
     if (a.length < 2) {
         return a;
     } else {
         var midpoint = parseInt(a.length / 2);
-
         var leftArr = a.slice(0, midpoint);
         var rightArr = a.slice(midpoint, a.length);
-        console.log(leftArr, '>>>', rightArr, ' ...', midpoint);
-        Merge(MergeSort(leftArr), MergeSort(rightArr));
+        return Merge(MergeSort(leftArr), MergeSort(rightArr));
     }
 }
 
 function Merge(l, h) {
     var valueToBeStored = [];
-    console.log(l, ';;;', h)
-    while (l.length && h.length) {
+    while (l != undefined && h != undefined && l.length && h.length) {
         if (l[0] < h[0]) {
             valueToBeStored.push(l[0]);
             l.shift();
@@ -26,16 +20,16 @@ function Merge(l, h) {
             h.shift();
         }
     }
-    while (l.length) {
+    while (l != undefined && l.length) {
         valueToBeStored.push(l[0]);
         l.shift();
     }
-    while (h.length) {
+    while (h != undefined && h.length) {
         valueToBeStored.push(h[0]);
         h.shift();
     }
-    console.log(valueToBeStored)
     return valueToBeStored;
 }
-
-MergeSort(a);
+// var a = [340, 1, 3, 3, 76, 23, 4, 12, 122, 7642, 646];
+var a = [12, 11, 13, 5, 6, 7];
+console.log(MergeSort(a));
