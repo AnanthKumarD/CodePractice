8f0fdcb04b45f96732ea760b99d8079f42166e5b
andodda <ananthkumard89@gmail.com>
2020-05-27 10:09:38 +0530

    Code

---
+++ leetCode/reverseNumber.py
@@ -0,0 +1,26 @@
+#class Solution(object):
+import math
+def reverse(x):
+    val = str(x);
+    flag = 0;
+    if x < 0:
+        flag = 1;
+        val = val.split("-")[1];
+
+    b = "";
+    for n in range(len(val),0,-1):
+        b += val[n-1];
+
+    if flag == 1:
+        b = '-'+b;
+        return int(b) < -math.pow(2,31) and 0 or int(b);
+    else:
+        return int(b) < (math.pow(2, 31) -1) and int(b) or 0;
+        
+print(reverse(-123));
+print(reverse(1123));
+print(reverse(1534236469));
+print(reverse(-8463847412));
+
+
+print(dir(__builtins__))
